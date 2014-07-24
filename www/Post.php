502c70533153009dd999fa94507232ed35f14d9b
Domcallaghan <lucas.lepere27@gmail.com>
2014-07-24 15:17:01 +0200

    LLE - change the post file

--- www/Post.php
+++ www/Post.php
@@ -9,14 +9,24 @@
 		private $id;
 		private $status;
 
-		private function __construct()
+		private function __construct($post_message, $post_user, $post_title, $post_id)
 		{
-
+			$this->message = htmlspecialchars($post_message);
+			$this->user = htmlspecialchars($post_user);
+			$this->title = htmlspecialchars($post_title);
+			$this->id = $post_id;
 		}
 
-		public function checkForm()
+		public function checkMessage($post_message)
 		{
-
+			if (strlen($post_message) <= 300) 
+			{
+				return true;
+			}
+			else 
+			{
+				return false;
+			}
 		}		
 		// Return the post in a html division
 		public function showMessage()
