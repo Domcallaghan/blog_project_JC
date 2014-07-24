a7add6a05da8838d5f94caf37c153f1b265587de
Alexisquicode <alexis.barreau@viacesi.fr>
2014-07-24 11:55:00 +0200

    ABA - Add class Database with its methods declarations

---
+++ www/db_connect.php
@@ -0,0 +1,31 @@
+<?php
+	class Database {
+	
+		private function _connectDB(){
+		
+		}
+		public function getValidateMessages(){
+		
+		}
+		
+		public function getUnvalidateMessages(){
+		
+		}
+		
+		public function getWaitingForValidationMessages(){
+		
+		}
+		
+		public function setNewMessage(){
+	
+		}
+		
+		public function validateMessage(){
+		
+		}
+		
+		public function unvalidateMessage(){
+		
+		}
+	}
+?>
